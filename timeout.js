c402fcca7a2cdba4ca3df3cf243390c048e56efb
hetanhong <543039822@qq.com>
2018-02-26 10:52:10 +0800

    test

--- timeout.js
+++ timeout.js
@@ -1,32 +1,3 @@
 /**
  * Created by DELL on 2018/1/15.
  */
-var canJump = function (nums) {
-    var len = nums.length;
-    if (len <= 1) {
-        return true
-    }
-    if (nums[0] == 0) {
-        return false
-    }
-    var obj = {
-        index: 0,
-        value: nums[0]
-    }
-    for (var i = 1; i < len; i++) {
-        if (i == len - 1) {
-            return true
-        }
-        obj.value--;
-        var num = nums[i];
-        if (num > obj.value) {
-            obj.value = num;
-            obj.index = i;
-        }
-        if (obj.value <= 0 && num == 0) {
-
-            return false
-        }
-    }
-    return true;
-};
