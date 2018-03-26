ff0621d0bb67ea0a98cc5bb42086315d04a2c49f
hetanhong <543039822@qq.com>
2018-03-26 17:55:01 +0800

    add origin

---
+++ frontend-origin.js
@@ -0,0 +1,37 @@
+// author: DELL
+// created:2018/3/26 17:48
+export default {
+    cloud: {
+        test: 'open-test.zj-wm.cn',
+        prod: 'open.zj-wm.cn'
+    },
+    'cloud-h5': {
+        test: 'open-h5-test.zj-wm.cn',
+        prod: 'open-h5.zj-wm.cn'
+    },
+    'issuer-pc': {
+        test: 'issuer-test.zj-hf.cn',
+        prod: 'issuer.zj-hf.cn'
+    },
+    fop: {
+        test: 'fop-web-test.zj-hf.cn',
+        prod: 'fop.zj-hf.cn'
+    },
+    ifa: {
+        test: 'ifa-weixin-test.zj-hf.cn',
+        prod: 'ifa.zj-hf.cn'
+    },
+    kop: {
+        test: 'kop-web-test.zj-hf.cn'
+    },
+    weixin: {
+        test: 'https://static-test.zj-hf.cn',
+        stage: 'https://pre-zj-static.zj-hf.cn',
+        prod: 'https://zj-static.zj-hf.cn'
+    },
+    'm-site': {
+        test: 'https://static-test.zj-wm.cn',
+        stage: 'https://pre-zj-static.zj-wm.cn',
+        prod: 'https://marketing.zj-wm.cn'
+    }
+};
